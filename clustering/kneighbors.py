911f74c7daba8cc33cbcf7d4d494b877f0df5e28
elenaxubrown <32278352+elenaxubrown@users.noreply.github.com>
2018-03-21 20:51:07 -0400

    Update kneighbors.py

--- clustering/kneighbors.py
+++ clustering/kneighbors.py
@@ -1,9 +1,3 @@
-"""
-    This is a file you will have to fill in.
-
-    It contains helper functions required by K Nearest Neighbors method
-    Note: get_neighbors_indices and get_response are designed to be called in sequence by models.py
-"""
 import numpy as np
 import operator
 
@@ -16,13 +10,10 @@ def euclidean_distance(input1, input2):
     :return: euclidean distance between input1 and input2, a float number
     """
 
-    #dist = np.linalg.norm(input1-input2)
     input1 = np.array(input1)
     input2 = np.array(input2)
     d = np.sqrt(np.sum((input1-input2)**2))
     return d
-    # Recall the definition of Euclidean distance for two vectors x,y
-    # d = sqrt(sum(x_i - y_i)^2)
 
 def get_neighbors_indices(training_inputs, test_instance, k):
     """
@@ -32,16 +23,8 @@ def get_neighbors_indices(training_inputs, test_instance, k):
     :param training_inputs: inputs of training data, a 2D Python list
     :param test_instance: an instance input of test data, a Python list
     :param k: number of neighbors used, an int
-    :return: a Python list of indices of k closest neighbors from the training set for a given test instance
     """
 
-    #indice = [[]] * len(test_instance)
-    #for i in range(len(test_instance)):
-     #   dist = [] * len(training_inputs)
-      #  for j in range(len(training_inputs)):
-       #     dist[j] = euclidean_distance(test_instance[i], training_inputs[j])
-        #    dist.sort()
-        #indice[i] = dist[:k]
     dist = []
     for i in range(len(training_inputs)):
         distance = euclidean_distance(test_instance, training_inputs[i])
@@ -60,11 +43,6 @@ def get_response(training_labels, neighbor_indices):
     :return: the class/label with the highest vote, an int
     """
 
-    #neighbor_indices = np.array(neighbor_indices)
-    #freq = [] * len(neighbor_indices)
-    #for i in range(len(neighbor_indices)):
-    #    freq[i] = np.bincount(neighbor_indices[i]).argmax()
-    #return freq
     vote = {}
     for i in neighbor_indices:    
         if training_labels[i] in vote:
